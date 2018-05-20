79a2cf7aa4abc750e790d9d88eb9d28eda1b0c9d
Michael Angerman <stormasm@gmail.com>
2018-05-20 07:32:51 -0700

    mw

--- src/containers/FileContainer.js
+++ src/containers/FileContainer.js
@@ -1,37 +1,36 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { addToCart } from '../actions'
-import { getVisibleProducts } from '../reducers/products'
-import ProductItem from '../components/ProductItem'
-import ProductsList from '../components/ProductsList'
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getVisibleFiles } from "../reducers/files";
+import ProductItem from "../components/ProductItem";
+import ProductsList from "../components/ProductsList";
 
 const ProductsContainer = ({ products, addToCart }) => (
   <ProductsList title="Products">
-    {products.map(product =>
+    {products.map(product => (
       <ProductItem
         key={product.id}
         product={product}
-        onAddToCartClicked={() => addToCart(product.id)} />
-    )}
+        onAddToCartClicked={() => addToCart(product.id)}
+      />
+    ))}
   </ProductsList>
-)
+);
 
 ProductsContainer.propTypes = {
-  products: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    inventory: PropTypes.number.isRequired
-  })).isRequired,
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+      inventory: PropTypes.number.isRequired
+    })
+  ).isRequired,
   addToCart: PropTypes.func.isRequired
-}
+};
 
 const mapStateToProps = state => ({
-  products: getVisibleProducts(state.products)
-})
+  products: getVisibleFiles(state.products)
+});
 
-export default connect(
-  mapStateToProps,
-  { addToCart }
-)(ProductsContainer)
+export default connect(mapStateToProps)(ProductsContainer);
