2203c908b6bffa053106eb3989ca22221760a31b
Michael Angerman <stormasm@gmail.com>
2018-05-22 15:52:18 -0700

    mw

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,3 +14,42 @@ export const getAllFiles = () => dispatch => {
     dispatch(receiveFiles(template))
   })
 }
+
+// all new code should go below here
+
+function requestFiles() {
+  return {
+    type: types.REQUEST_FILES
+  }
+}
+
+const receiveFiles = files => ({
+  type: types.RECEIVE_FILES,
+  files: files
+})
+
+export function fetchFilesIfNeeded() {
+  return (dispatch, getState) => {
+    if (shouldFetchFiles(getState())) {
+      return dispatch(fetchFiles())
+    }
+  }
+}
+
+function shouldFetchFiles(state) {
+  const files = state.files;
+  if (files.isFetching) {
+    return false
+  } else {
+    return true
+  }
+}
+
+function fetchFiles() {
+  return dispatch => {
+    dispatch(requestFiles())
+    return fetch("https://api.github.com/repos/stormasm/checkbox-file-selector/contents/src/data/repos")
+      .then(response => response.json())
+      .then(json => dispatch(receiveFiles(json)))
+  }
+}
--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -1,2 +1,3 @@
 export const ADD_TO_CART = 'ADD_TO_CART'
+export const REQUEST_FILES = 'REQUEST_FILES'
 export const RECEIVE_FILES = 'RECEIVE_FILES'
